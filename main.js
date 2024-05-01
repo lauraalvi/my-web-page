9c8674a974d9f26c530266742aaf532fb8e6a18b
lauraalvi <laav2018@gmail.com>
2024-04-30 20:10:37 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('botonTextEncryptor').addEventListener('click', function() {
+    window.location.href = 'https://lauraalvi.github.io/Encriptadorfinal/'; 
+});
+
+document.getElementById('botonAluraPlay').addEventListener('click', function() {
+    window.location.href = 'https://lauraalvi.github.io/AluraPlay/'; 
+});
